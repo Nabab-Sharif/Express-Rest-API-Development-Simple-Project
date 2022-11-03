287db509af97da930bdd26466bf320eebbfff8dc
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-11-03 11:22:19 +0600

    110-User Registration ,User Profile Create

---
+++ src/models/ProfileModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const DataSchema = mongoose.Schema({
+  FirstName: { type: String },
+  LastName: { type: String },
+  UserName: { type: String },
+  EmailAddress: { type: String },
+  Password: { type: String },
+  City: { type: String },
+  MobileNumber: { type: String }
+
+}, { versionKey: false });
+
+
+const ProfileModel = mongoose.model("Profile", DataSchema);
+module.exports = ProfileModel;
+
