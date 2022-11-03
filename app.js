d1da570bca4cc16dd8facae2daef43f6ae88e264
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-11-03 08:42:58 +0600

    108-MongoDB Database connection, Routing End point implemnet, Undefined Route Implement and listening implement

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = new express();
 const router = require("./src/routes/api.route");
 const bodyParser = require("body-parser");
 
+
 //Security Middleware Library Import
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
@@ -25,10 +26,34 @@ app.use(xss());
 app.use(hpp());
 app.use(cors());
 
+
 //Body Parser Implement
 app.use(bodyParser.json())
+
 
 //Request Rate Limit Implement
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
 app.use(limiter);
 
+
+
+//MongoDB Database Connection
+const URI = "mongodb://127.0.0.1:27017/Todo";
+const OPTION = { user: '', pass: '' };
+mongoose.connect(URI, OPTION, (error) => {
+  console.log("Connection Success");
+  console.log(error);
+})
+
+
+//Routing Implement 
+app.use("/api/v1", router);
+
+
+//Undefined Route Implement
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "fail", data: "Not Found" });
+})
+
+
+module.exports = app;
